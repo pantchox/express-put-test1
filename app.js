de37bc21eb72b93ff55fcda3320bccc2f18d150d
RYeah Sh <7011604+pantchox@users.noreply.github.com>
2022-08-19 21:36:09 +0300

    update port

--- app.js
+++ app.js
@@ -20,6 +20,6 @@ app.put("/api/files", (req, res) => {
   return res.send("ok!");
 });
 
-var listener = app.listen(8080, function () {
+var listener = app.listen(process.env.PORT || 8080, function () {
   console.log("Listening on port " + listener.address().port);
 });
