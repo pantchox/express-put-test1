403257f626b77e1f740757b5f97674d190e3cfeb
RYeah Sh <7011604+pantchox@users.noreply.github.com>
2022-08-24 16:06:30 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 var express = require("express");
 var path = require("path");
-var cookieParser = require("cookie-parser");
+//var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+//var indexRouter = require("./routes/index");
+//var usersRouter = require("./routes/users");
 
 var app = express();
 
 app.use(logger("dev"));
 app.use(express.json({limit: "1000MB"}));
 app.use(express.urlencoded({ extended: false, limit: "1000MB" }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+//app.use(cookieParser());
+//app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+//app.use("/", indexRouter);
+//app.use("/users", usersRouter);
 app.put("/api/files", (req, res) => {
   return res.end("ok!");
 });
