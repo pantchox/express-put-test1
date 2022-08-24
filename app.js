ee2cb86d1858aad7e57047fa3b15d3ff3faa3c11
RYeah Sh <7011604+pantchox@users.noreply.github.com>
2022-08-24 15:47:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var usersRouter = require("./routes/users");
 var app = express();
 
 app.use(logger("dev"));
-app.use(express.json());
+// app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
