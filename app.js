3178acc05a4d4df1585f43fba281eda292989738
RYeah Sh <7011604+pantchox@users.noreply.github.com>
2022-08-24 23:05:24 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,8 +9,11 @@ var logger = require("morgan");
 var app = express();
 
 app.use(logger("dev"));
-app.use(express.json({limit: "1000MB"}));
-app.use(express.urlencoded({ extended: false, limit: "1000MB" }));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
+
+//app.use(express.json({limit: "1000MB"}));
+//app.use(express.urlencoded({ extended: false, limit: "1000MB" }));
 //app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, "public")));
 
