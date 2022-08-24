770af938e18733b7e1b8848f18803dbbafca4829
RYeah Sh <7011604+pantchox@users.noreply.github.com>
2022-08-24 16:02:31 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ var usersRouter = require("./routes/users");
 var app = express();
 
 app.use(logger("dev"));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
+app.use(express.json({limit: "1000MB"}));
+app.use(express.urlencoded({ extended: false, limit: "1000MB" }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
